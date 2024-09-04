c9c4d07791d655114cbe39f20653acd69c372106
JahshuaG <joshnutall@gmail.com>
2024-09-04 16:28:46 -0500

    "config updated"

---
+++ config.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    API_URL: "https://cnt-2d7f47b5-8b01-49ac-86e3-cb50588abbd3.containerhub.tripleten-services.com",
+    API_KEY: "e255fbf8-362a-4b73-aec6-2c777474a1b5"}

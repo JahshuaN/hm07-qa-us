384db75f780250090f5f02d3a0f56119cae1a108
Jahshua <105691407+JahshuaN@users.noreply.github.com>
2024-09-03 21:44:04 -0500

    Update postHandlers.test.js

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -1,20 +1,47 @@
 // eslint-disable-next-line no-undef
+//Test 1
 const config = require('../config');
 
 const requestBody = {
-    // put your body here
+    "productsList": [
+    {
+      "id": 5,
+      "quantity": 1
+    }
+  ]
 }
 
-test('', async () => {
+test('Status code should be 201', async () => {
+	let actualStatus;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/orders`, {
 			method: 'POST',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
+		actualStatus = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatus).toBe(201);
+});
+
+//Test 2
+test('Response body should contain...', async () => {
+	let actualResponseBody;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		actualResponseBody = await response.json()
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualResponseBody["wareHouse"]).toBe("Fresh Food");
 });

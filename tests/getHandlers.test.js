c21cda5d67344defdedcfd1f530315c7d7b4b357
Jahshua <105691407+JahshuaN@users.noreply.github.com>
2024-09-03 21:43:45 -0500

    Update getHandlers.test.js

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,26 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
-	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
-	} catch (error) {
-		console.error(error);
-	}
+//First Test Case
+test('Should return 200 status code...', async () => {
+    let actualStatus;
+    try {
+        const response = await fetch(`${config.API_URL}/api/v1/kits/1`)
+        actualStatus = response.status;
+    } catch (error) {
+        console.error(error);
+    }
+    expect(actualStatus).toBe(200);
+});
+
+//Second Test Case
+test('Body should contain respone for GET...', async () => {
+    let actualResponseBody;
+    try {
+        const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
+        actualResponseBody = await response.json()
+    } catch (error) {
+        console.error(error);
+    }
+    expect(actualResponseBody.name).toBe("For picnic");
 });

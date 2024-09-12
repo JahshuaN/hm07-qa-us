a6af686ba1a63270f5a60b17f680894fc9e2ca49
Jahshua <105691407+JahshuaN@users.noreply.github.com>
2024-09-11 19:43:22 -0500

    Update deleteHandlers.test.js

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -2,26 +2,29 @@ const fetch = require('node-fetch');
 const config = require('../config');
 
 test('Should return status 200', async () => {
+	let actualStatus;
+	let responseBody;
     try {
         const response = await fetch(`${config.API_URL}/api/v1/kits/1`, {
             method: 'DELETE'
         });
-        const actualStatus = response.status;
-        expect(actualStatus).toBe(200);
+        actualStatus = response.status;
+	responseBody = await response.json();
     } catch (error) {
         console.error('Error:', error);
     }
+	expect(actualStatus).toBe(200);
 });
 
-test('Should be true...', async () => {
+test('Response body should contain {ok: true}', async () => {
 	let actualResponseBody;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/1`, {
-			method: 'DELETE'
-		});
-			const responseBody = await response.json();
-			expect(responseBody).toEqual({ ok: true });
-		} catch (error) {
-			console.error('Error:', error);
-		}
+	const response = await fetch(`${config.API_URL}/api/v1/kits/1`, {
+		method: 'DELETE'
 	});
+	    actualResponseBody = await response.json();
+	} catch (error) {
+		console.error('Error:', error);
+	}
+	expect(responseBody).toEqual({ ok: true });
+});
